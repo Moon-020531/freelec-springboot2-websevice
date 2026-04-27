b3e30eecea308325ec93e6da3da0a39ce519072b
Moon kyuseok <msk7676@naver.com>
2026-04-27 11:34:46 +0900

    Initial commit

---
+++ src/main/java/com/jojoldu/book/freelecspringboot2websevice/FreelecSpringboot2WebseviceApplication.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.freelecspringboot2websevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelecSpringboot2WebseviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreelecSpringboot2WebseviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jojoldu/book/freelecspringboot2websevice/FreelecSpringboot2WebseviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.freelecspringboot2websevice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelecSpringboot2WebseviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
