9cbc81138ef430b6f8e9b0cfe888f35bd07b0574
Moon kyuseok <msk7676@naver.com>
2026-04-28 15:45:09 +0900

    [Day02-practice] feat: PostsRepository.findByTitleContaining 추가

--- src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
     List<Posts> findByAuthor(String author);
+    List<Posts> findByTitleContaining (String keyword);
 }
